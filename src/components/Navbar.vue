d2576e6d80db66338fc476764b3145d58ca8cb4b
jchumillas <desarrollo5@gnuinux.com>
2019-03-24 20:53:23 +0100

    fixed navbar collapse

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,20 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Finizens</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg navbar-light bg-light" v-bind:class=" { 'navbarOpen': show }">
+  <a class="navbar-brand" href="#">Finizens App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" v-on:click="toggleNavbar">
       <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <router-link to="/"><a class="nav-link">Contacts</a></router-link>
-        </li>
-        <li class="nav-item">          
-          <router-link to="/load-file"><a class="nav-link">Load File</a></router-link>
-        </li>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" v-bind:class="{ 'show': show }">
+      <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+              <router-link to="/"><a class="nav-link">Contacts</a></router-link>
+          </li>
+          <li class="nav-item">
+              <router-link to="/load-file"><a class="nav-link">Load File</a></router-link>
+          </li>
       </ul>
-    </div>
-  </nav>
+  </div>
+</nav>
 </template>
 
 <script>
@@ -22,6 +22,21 @@ export default {
   name: "Navbar",
   props: {
     msg: String
+  },
+  data(){
+    return{
+      show: true
+    }
+  },
+  methods: {
+    toggleNavbar() {
+      this.show = !this.show;
+    }
   }
 };
 </script>
+<style>
+  nav{
+    margin-bottom: 25px;
+  }
+</style>
